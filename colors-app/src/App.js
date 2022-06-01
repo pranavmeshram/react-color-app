4d96001ce2b797b8eef273046e40e995f8479f9e
pranavmeshram <gcpranav@gmail.com>
2022-06-01 11:37:05 +0530

    added routes 1

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -4,13 +4,35 @@ import seedColors from './seedColors';
 
 import { generatePallete } from "./colorHelpers";
 
+import { Route, Switch } from "react-router-dom"
+
 class App extends Component {
+
+  findPalette(id) {
+    return seedColors.find(function (palette) {
+      return palette.id === id
+    });
+  }
+
   render() {
-    // console.log('generatePallete-', generatePallete(seedColors[4]))
     return (
-      <div>
-        <Palette palette={generatePallete(seedColors[4])} />
-      </div>
+      <Switch>
+        <Route exact path="/" render={() => <h1>Palette List goes here</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={routeProps => (
+            <Palette palette={generatePallete(
+              this.findPalette(routeProps.match.params.id)
+            )}
+            />
+          )}
+        />
+      </Switch>
+
+      // <div>
+      //   <Palette palette={generatePallete(seedColors[4])} />
+      // </div>
     )
   }
 }
