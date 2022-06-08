2ef44d54e35e3313d40542f9ad5e748e9430f676
pranavmeshram <gcpranav@gmail.com>
2022-06-08 18:49:42 +0530

    add draggable colorBox

---
+++ colors-app/src/components/DraggableColorBox.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { withStyles } from '@material-ui/styles';
+// import styles from "./styles/MiniPaletteStyles";
+
+const styles = {
+    root: {
+        width: "20%",
+        height: "25%",
+        margin: "0 auto",
+        display: "inline-block",
+        position: "relative",
+        cursor: "pointer",
+        marginBottom: "-4.5px"
+    }
+};
+
+function DraggableColorBox(props) {
+    return (
+        <div
+            className={props.classes.root}
+            style={{ backgroundColor: props.color }}
+        >
+            {props.color}
+        </div>
+    )
+}
+
+export default withStyles(styles)(DraggableColorBox);
